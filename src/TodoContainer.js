ecd95b5918215ef1f6efa92626602c85945f31ae
ivaan1992 <ivaan.lg.92@gmail.com>
2022-03-24 12:24:31 -0600

    Add the parent component and start using it

--- src/TodoContainer.js
+++ src/TodoContainer.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 
-class TodoContainer extends React.Component {
-  render() {
-    return (
-      <div>
-          <h1>Hello from Create React App</h1>
-          <p>Im in a React Component!!</p>
-      </div>
-    );
-  }
-}
+const TodoContainer = () => (
+  <div>
+    <h1>Testing Parent Component</h1>
+    <p>I wrote this is the parent component</p>
+    <p>I wrote this is the parent component</p>
+    <p>I wrote this is the parent component</p>
+    <p>I wrote this is the parent component</p>
+  </div>
+);
 
 export default TodoContainer;
